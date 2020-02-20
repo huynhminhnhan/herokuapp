9fecbb1dcc06a3301f63bf8999591c9d420ace2f
HuynhMinhNhan <huynhminhnhan69@gmail.com>
2020-02-20 23:23:59 +0700

    update

--- app/Http/Controllers/FbToolController.php
+++ app/Http/Controllers/FbToolController.php
@@ -46,6 +46,15 @@ class FbToolController extends Controller
         return view('home');
     }
     public function webhook() {
-      echo 'hihi';
+      if (isset($_GET['hub_mode']) && isset($_GET['hub_challenge']) && isset($_GET['hub_verify_token'])) {
+        if ($_GET['hub_verify_token'] == 'my_verify_token')
+        echo $_GET['hub_challenge'];
+    } else {
+      $feedData = file_get_contents('php://input');
+      $data = json_decode($feedData);
+      echo $data;
     }
+    http_response_code(200);
+  }
+   
 }
