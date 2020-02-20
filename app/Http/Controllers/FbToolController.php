b6fca0b0648a2cc6081437b0e1e2e1d64a2b1f1e
HuynhMinhNhan <huynhminhnhan69@gmail.com>
2020-02-20 23:11:31 +0700

    update

--- app/Http/Controllers/FbToolController.php
+++ app/Http/Controllers/FbToolController.php
@@ -46,23 +46,6 @@ class FbToolController extends Controller
         return view('home');
     }
     public function webhook() {
-      $appsecret = 'e4c7e05848fc07fb3aff81aadcb3cf4b';
-      $raw_post_data = file_get_contents('php://input');
-      $header_signature = header('X-Hub-Signature');
-
-      $expected_signature = hash_hmac('sha1', $raw_post_data, $appsecret);
-
-$signature = '';
-if(
-    strlen($header_signature) == 45 &&
-    substr($header_signature, 0, 5) == 'sha1='
-  ) {
-  $signature = substr($header_signature, 5);
-}
-if (hash_equals($signature, $expected_signature)) {
-  echo('SIGNATURE_VERIFIED');
-}
-echo('fail');
-
+      echo 'hihi';
     }
 }
